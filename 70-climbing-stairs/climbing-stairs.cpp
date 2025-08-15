ad195a2b1f2e7e33ba097136027d03f8814e6010
Sahil mengji <135805311+sahil-mengji@users.noreply.github.com>
2025-08-16 02:06:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.50%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        vector<int> a(n+1);
+
+        a[0]=1;
+        a[1]=2;
+
+        for(int i=2;i<n;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+
+        return a[n-1];
+
+
+    }
+};
