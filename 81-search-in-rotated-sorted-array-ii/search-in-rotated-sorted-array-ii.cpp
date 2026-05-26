b8446ff18fb7fe9663dc904948bad4fdbce03d52
Sahil mengji <135805311+sahil-mengji@users.noreply.github.com>
2026-05-26 16:38:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (95.25%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,45 +1,34 @@
-
 class Solution {
 public:
-    bool search(vector<int>& arr, int k) {
-    int n = arr.size();
-    int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
+    bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
 
-       
-        if (arr[mid] == k) return true;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-       
-        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
-            low = low + 1;
-            high = high - 1;
-            continue;
-        }
+            if (nums[mid] == target)
+                return true;
 
-       
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= k && k <= arr[mid]) {
-              
-                high = mid - 1;
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
             }
+           
+            else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+       
             else {
-              
-                low = mid + 1;
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
-        else { 
-            if (arr[mid] <= k && k <= arr[high]) {
-               
-                low = mid + 1;
-            }
-            else {
-               
-                high = mid - 1;
-            }
-        }
+
+        return false;
     }
-    return false;
-    }
-	
 };
